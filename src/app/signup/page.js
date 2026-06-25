d7387265e58f6a3b25d18767a7d27c2484653cc8
amaldev7510 <aromal321654@gmail.com>
2026-06-25 12:09:08 +0530

    Updated signup page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,75 +1,103 @@
 "use client";
+
+import Link from "next/link";
 import { useState } from "react";
-import supabase from "../config/supabaseClient"; 
-import"../css/signup.css";
+import { useRouter } from "next/navigation";
+import supabase from "../config/supabaseClient";
+import "../css/signup.css";
+
 export default function Signup() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [full_Name, setFullName] = useState("");
-  const [formErrors, setFormErrors] = useState("");
-  const handleSubmit= async (e) =>{
-    e.preventDefault()
-    if(!full_Name || !email || !password) {
-      setFormErrors('please fill all the fields correctly')
-      return
-    }
-    const {data, error} = await  supabase
-    .from('User')
-    .insert([{full_Name,email,password}])
 
-    if (error) {  
-      console.log(error)
-      setFormErrors('please fill all the fields correctly')
-    }
-    if (data) {
-      console.log(data)
-      setFormErrors(null)
-    }
-  }
-  return (
-    <>
-    <nav className="navbar">
-  
-        <Link href="/" className="navLink">
-          Home
-        </Link>
-        
-      </nav>
-    <div className="container">
-      <h1 className="heading">Signup</h1>
+const router = useRouter();
 
-      <form onSubmit={handleSubmit}>
-        <input
-         className="input"
-          type="text"
-          placeholder="Full Name"
-          value={full_Name}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-         <br/>
-        <input
-          className="input"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
-        />
-          <br/>
-        <input
-          className="input"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e)=>setPassword(e.target.value)}
-        />
-            <br/>
-        <button type="submit" className="button">
-             sign up
-        </button>
-        <br/>
-        {formErrors && <p className="error">{formErrors}</p>}
-      </form>
-    </div>
-    </>
-  );
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [full_Name, setFullName] = useState("");
+const [formErrors, setFormErrors] = useState("");
+
+const handleSubmit = async (e) => {
+
+e.preventDefault();
+
+if (!full_Name || !email || !password) {
+  setFormErrors("Please fill all fields");
+  return;
+}
+
+const { error } = await supabase
+  .from("User")
+  .insert([{full_Name,email,password, }, ]);
+
+if (error) {
+  setFormErrors(error.message);
+  return;
+}
+
+alert("Signup Successful");
+
+router.push("/blogs");
+
+};
+
+return (
+<> <nav className="navbar"> <Link href="/" className="navLink">
+Home </Link> </nav>
+
+
+  <div className="container">
+
+    <h1 className="heading">Signup</h1>
+
+    <form onSubmit={handleSubmit}>
+
+      <input
+        className="input"
+        type="text"
+        placeholder="Full Name"
+        value={full_Name}
+        onChange={(e) =>
+          setFullName(e.target.value)
+        }
+      />
+
+      <input
+        className="input"
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) =>
+          setEmail(e.target.value)
+        }
+      />
+
+      <input
+        className="input"
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) =>
+          setPassword(e.target.value)
+        }
+      />
+
+      <button
+        type="submit"
+        className="button"
+      >
+        Sign Up
+      </button>
+
+      {formErrors && (
+        <p className="error">
+          {formErrors}
+        </p>
+      )}
+
+    </form>
+
+  </div>
+</>
+
+
+);
 }
